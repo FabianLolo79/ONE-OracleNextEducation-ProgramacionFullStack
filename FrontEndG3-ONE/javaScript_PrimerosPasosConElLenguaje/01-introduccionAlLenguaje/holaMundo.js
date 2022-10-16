3d9044ddcfe98fee5d6c96674fee7adcef44276c
Fabián Enrique García <fabianegl79@gmail.com>
2022-10-16 19:25:24 -0300

    Prácticas finalizadas

---
+++ FrontEndG3-ONE/javaScript_PrimerosPasosConElLenguaje/01-introduccionAlLenguaje/holaMundo.js
@@ -0,0 +1,15 @@
+console.log ("Mi primer programa");
+console.log (2+2);
+
+const nombre = "Fabián";
+console.log (nombre);
+const edad = 42;
+console.log (edad);
+
+const nombreMadre = "Silvia";
+const edadMadre= 66;
+const nombrePadre = "Néstor";
+const nombreHijo = "Fabián";
+
+console.log ("El nombre de mi madre es "+ nombreMadre);
+console.log ("La edad de mi madre es "+ edadMadre);
